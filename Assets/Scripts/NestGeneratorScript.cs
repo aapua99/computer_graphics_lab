8f7141e20823f24cbbe2961640ed785369903954
aapua99 <aapua99@gmail.com>
2019-09-29 13:44:48 +0300

    add lab_1

---
+++ Assets/Scripts/NestGeneratorScript.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NestGeneratorScript : MonoBehaviour
+{
+    public float Xsize = 13.5f;
+    public float Zsize = 6.5f;
+    public GameObject nestPrefab;
+    public Vector3 curPos;
+    public GameObject curNest;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!curNest)
+        {
+            GenerateRandomPosition();
+            curNest = GameObject.Instantiate(nestPrefab, curPos, Quaternion.identity);
+            curNest.tag = "Nest";
+        }
+
+    }
+
+    void GenerateRandomPosition()
+    {
+        curPos = new Vector3(Random.Range(Xsize * -1, Xsize), 0, Random.Range(Zsize * -1, Zsize));
+    }
+}
---
+++ Assets/Scripts/SnakeTailMovement.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SnakeTailMovement : MonoBehaviour
+{
+    private const string MAIN_SNAKE_TAG = "MainSnake";
+    public float speed;
+    public float rotationSpeed;
+    public GameObject tailTarget;
+    public Vector3 tailTargetVector;
+    public SnakeMovementScript snakeMovement;
+    void Start()
+    {
+        snakeMovement = GameObject.FindGameObjectWithTag(MAIN_SNAKE_TAG).GetComponent<SnakeMovementScript>();
+        tailTarget = snakeMovement.gameObject;
+        speed = snakeMovement.speed;
+        rotationSpeed = snakeMovement.rotationSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        speed = snakeMovement.speed;
+        tailTargetVector = tailTarget.transform.position;
+        transform.LookAt(tailTargetVector);
+        transform.position = Vector3.Lerp(transform.position, tailTargetVector, Time.deltaTime * speed);
+
+    }
+}
